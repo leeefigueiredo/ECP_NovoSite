b44aad898cf8451b5d45b5b11d18a169a2f6e058
leeefigueiredo <leeeandrofigueiredo@gmail.com>
2025-06-23 05:17:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// MENU HAMBURGER
+
+    const hamburger = document.getElementById('hamburger');
+    const menu = document.getElementById('menu');
+
+    hamburger.addEventListener('click', () => {
+      menu.classList.toggle('show');
+    });
